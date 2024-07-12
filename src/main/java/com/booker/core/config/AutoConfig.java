6194a7a013cf2aaad3cd9c7f57be849fe2e20ade
nyximos <nyximos@gmail.com>
2024-07-13 00:18:44 +0900

    AutoConfig 추가

---
+++ src/main/java/com/booker/core/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.booker.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.booker.core")
+public class AutoConfig {
+}
